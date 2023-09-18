c96d6224e6dbc9cfa54ed6af8ebd435792d8a6e6
CesarGorge <cesargorgeee@gmail.com>
2023-09-18 01:54:43 +0000

    todos done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,55 @@
+import { useState } from "react";
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="container" style={{ width: 400 }}>
+      <h1 style={{ color: "grey" }}>Todos</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            style={{ outline: "none" }}
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setTodos(todos.concat(inputValue));
+                setInputValue("");
+              }
+            }}
+            placeholder={todos.length === 0 ? "No tasks, add task." : "to do"}
+          ></input>
+        </li>
+        {todos.map((item, index) => (
+          <li
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <span>{item}</span>
+            <i
+              className="far fa-minus-square"
+              style={{
+                color: "grey",
+              }}
+              onClick={() =>
+                setTodos(
+                  todos.filter((t, currentIndex) => index !== currentIndex)
+                )
+              }
+            ></i>
+          </li>
+        ))}
+      </ul>
+      <div style={{ fontSize: 12, color: "grey" }}>
+        {todos.length ? todos.length + " item left" : ""}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
