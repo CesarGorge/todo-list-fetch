e6eda74b6770f40e4793ab863bab5ff6e7d89eeb
CesarGorge <cesargorgeee@gmail.com>
2023-09-18 02:57:08 +0000

    todo list with fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,66 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
 
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/alesanchezr")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setTodos(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  const agregarTarea = () => {
+    if (inputValue) {
+      const nuevaTarea = { label: inputValue, done: false };
+      const nuevosTodos = [...todos, nuevaTarea];
+      setInputValue("");
+      setTodos(nuevosTodos);
+    }
+  };
+
+  useEffect(() => {
+    actualizarServidor(todos);
+  }, [todos]);
+
+  const eliminarTarea = (indice) => {
+    const tareasActualizadas = todos.filter((_, i) => i !== indice);
+    setTodos(tareasActualizadas);
+  };
+
+  const eliminarTodasLasTareas = () => {
+    setTodos([]);
+  };
+
+  const actualizarServidor = (datos) => {
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/alesanchezr", {
+      method: "PUT",
+      body: JSON.stringify(datos),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => {
+        console.log(resp.ok);
+        console.log(resp.status);
+        return resp.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="container" style={{ width: 400 }}>
-      <h1 style={{ color: "grey" }}>Todos</h1>
+      <h1 style={{ color: "grey" }}>Tareas</h1>
       <ul>
         <li>
           <input
@@ -16,11 +70,14 @@ const Home = () => {
             value={inputValue}
             onKeyDown={(e) => {
               if (e.key === "Enter") {
-                setTodos(todos.concat(inputValue));
-                setInputValue("");
+                agregarTarea();
               }
             }}
-            placeholder={todos.length === 0 ? "No tasks, add task." : "to do"}
+            placeholder={
+              todos.length === 0
+                ? "No hay tareas, agrega una tarea."
+                : "por hacer"
+            }
           ></input>
         </li>
         {todos.map((item, index) => (
@@ -29,25 +86,23 @@ const Home = () => {
               display: "flex",
               justifyContent: "space-between",
             }}
+            key={index}
           >
-            <span>{item}</span>
+            <span>{item.label}</span>
             <i
               className="far fa-minus-square"
               style={{
                 color: "grey",
               }}
-              onClick={() =>
-                setTodos(
-                  todos.filter((t, currentIndex) => index !== currentIndex)
-                )
-              }
+              onClick={() => eliminarTarea(index)}
             ></i>
           </li>
         ))}
       </ul>
       <div style={{ fontSize: 12, color: "grey" }}>
-        {todos.length ? todos.length + " item left" : ""}
+        {todos.length ? todos.length + " tarea(s) restante(s)" : ""}
       </div>
+      <button onClick={eliminarTodasLasTareas}>Limpiar todas las tareas</button>
     </div>
   );
 };
